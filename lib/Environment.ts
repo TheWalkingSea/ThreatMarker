b009bc8c30353735fe84058742c6bce4086660ad
Austin McCallister <austinmccallister0@gmail.com>
2024-10-09 12:57:00 -0400

    Changed record to TaintedVariable
    Holds metadata for kind

--- lib/Environment.ts
+++ lib/Environment.ts
@@ -3,7 +3,7 @@ import { DeclarationException } from "./DeclarationException";
 
 export class Environment {
 
-    record: Map<string, TaintedLiteral>;
+    record: Map<string, TaintedVariable>;
     parent: Environment | null;
 
     constructor(record = new Map(), parent = null) {
@@ -11,30 +11,34 @@ export class Environment {
         this.parent = parent;
     }
 
-    declare(name: string): void {
+    declare(name: string, kind: string): void {
         if (this.record.has(name)) throw new DeclarationException(name);
         this.record.set(name, {
             value: undefined,
-            isTainted: false
+            isTainted: false,
+            kind: kind
         }); // Assumed to be untainted
     }
 
     assign(name: string, { value, isTainted }: TaintedLiteral): void {
         if (!this.record.has(name)) throw new ReferenceException(name);
 
+        let entry = this.record.get(name) as TaintedVariable;
+
         if (isTainted === undefined) {
-            isTainted = (this.record.get(name) as TaintedLiteral).isTainted;
+            isTainted = entry.isTainted;
         }
 
         this.record.set(name, {
             value: value,
-            isTainted: isTainted
+            isTainted: isTainted,
+            kind: entry.kind // Inherit kind - Only defined at declaration
         });
     }
 
-    resolve(name: string): TaintedLiteral {
+    resolve(name: string): TaintedVariable {
         if (this.record.has(name)) {
-            return this.record.get(name) as TaintedLiteral; // Checked if it exists above
+            return this.record.get(name) as TaintedVariable; // Checked if it exists above
         } else if (!this.parent) {
             throw new ReferenceException(name)
         } else {
