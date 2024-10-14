b9a856984ffbb6a4f049e54ddb48feadb2ee1741
Austin McCallister <austinmccallister0@gmail.com>
2024-10-14 16:13:27 -0400

    Added string as a possible Literal/Value

--- utils/to_value.ts
+++ utils/to_value.ts
@@ -2,7 +2,9 @@ import * as t from '@babel/types';
 
 
 export function Value(value: any): t.Literal {
-    if (typeof value === 'number') {
+    if (typeof value === 'string') {
+        return t.stringLiteral(value);  // Create a numeric literal
+    } else if (typeof value === 'number') {
         return t.numericLiteral(value);  // Create a numeric literal
     } else if (typeof value === 'boolean') {
         return t.booleanLiteral(value);  // Create a boolean literal
