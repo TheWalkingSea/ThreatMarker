fa881599acc736b5f3af32df8ce6a2760b2a626b
Austin McCallister <austinmccallister0@gmail.com>
2024-10-14 15:49:48 -0400

    Updated TaintedLiteral to support Nodes for tainted vals

--- d.ts
+++ d.ts
@@ -1,4 +1,10 @@
-declare interface TaintedLiteral {
-    value?: any,
-    isTainted: Boolean | undefined
+import { Node, Expression } from '@babel/types';
+
+declare global {
+    interface TaintedLiteral {
+        value?: any,
+        node?: Expression,
+        isTainted: Boolean | undefined
+    }
+    
 }
