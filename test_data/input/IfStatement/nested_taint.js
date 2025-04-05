0be49ce2e5a1022f5617bab8b8c5d2247fae630e
Austin McCallister <austinmccallister0@gmail.com>
2025-04-05 14:27:11 -0400

    Nested Taint Test

---
+++ test_data/input/IfStatement/nested_taint.js
@@ -0,0 +1,29 @@
+var a = 2;
+var c = document;
+
+if (c > 0) {
+    a = 4;
+} else if (c < 0) {
+    a = 2;
+} else {
+    if (4 % 2 == 0) {
+        a = 3;
+    }
+}
+
+
+a;
+
+
+if (c > 0) {
+    a = 4;
+} else if (c < 0) {
+    a = 2;
+} else {
+    if (4 % 2 == 1) {
+        a = 3; 
+    }
+}
+
+
+a;
---
+++ test_data/output/IfStatement/nested_taint.js
@@ -0,0 +1,26 @@
+var a = 2;
+var c = document;
+
+if (c > 0) {
+    a = 4;
+} else if (c < 0) {
+    a = 2;
+} else {
+    {
+        a = 3;
+    }
+}
+
+
+a;
+
+
+if (c > 0) {
+    a = 4;
+} else if (c < 0) {
+    a = 2;
+} else {
+}
+
+
+a;
