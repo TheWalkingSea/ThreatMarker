79b5e258651158e453c55e4d244eff2928239518
Austin McCallister <austinmccallister0@gmail.com>
2025-05-17 11:06:13 -0400

    Update ExecutionContext.ts

--- lib/ExecutionContext.ts
+++ lib/ExecutionContext.ts
@@ -1,10 +1,11 @@
+import { is } from "@babel/types";
 import { Environment } from "./Environment";
 
 export class ExecutionContext {
     thisValue: any;
     environment: Environment;
 
-    constructor(thisValue: any, environment: Environment) {
+    constructor(thisValue: any, environment: Environment, is_breakable: boolean=false) {
         this.thisValue = thisValue;
         this.environment = environment;
     }
