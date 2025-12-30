034d94e545a9e03ef2d7e5696a046a2a80b55d4b
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2025-12-30 16:08:30 -0500

    Added ForStatement.tainted_condition

---
+++ test_data/input/ForStatement/tainted_condition.js
@@ -0,0 +1,7 @@
+// For loop with tainted test condition
+var count = 0;
+for (var i = 0; document; i++) {
+    count++;
+}
+count;
+i;
---
+++ test_data/output/ForStatement/tainted_condition.js
@@ -0,0 +1,6 @@
+var count = 0;
+for (var i = 0; document; i++) {
+  count++;
+}
+count;
+i;
