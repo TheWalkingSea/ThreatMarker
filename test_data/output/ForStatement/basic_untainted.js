21dddc0ad90dcd0c67c4f0236b49a8e393937092
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2025-12-31 12:14:40 -0500

    Fixed ForStatement.basic_untainted.js Test

--- test_data/output/ForStatement/basic_untainted.js
+++ test_data/output/ForStatement/basic_untainted.js
@@ -1,39 +1,61 @@
 var sum = 0;
 {
+  var i = 0;
   {
     sum += 0;
+    0;
+    i++;
   }
   {
     sum += 1;
+    1;
+    i++
   }
   {
     sum += 2;
+    2;
+    i++
   }
   {
     sum += 3;
+    3;
+    i++
   }
   {
     sum += 4;
+    4;
+    i++
   }
 }
 10;
 
 var countdown = 0;
 {
+  var j = 10;
   {
     countdown += 10;
+    10;
+    j--;
   }
   {
     countdown += 9;
+    9;
+    j--;
   }
   {
     countdown += 8;
+    8;
+    j--;
   }
   {
     countdown += 7;
+    7;
+    j--;
   }
   {
     countdown += 6;
+    6;
+    j--;
   }
 }
 40;
