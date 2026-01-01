8d0eb2b5f734fd417c77b4c3454da7c5fc50365a
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2026-01-01 17:45:09 -0500

    Added ContinueStatement.tainted_4.js

---
+++ test_data/input/ContinueStatement/tainted_4.js
@@ -0,0 +1,17 @@
+var a = 3;
+var b = document;
+var c = 5;
+
+while (b > 0) {
+    a;
+
+    if (a == 1) {
+        c;
+        continue;
+        c -= 2;
+    }
+
+    a -= 1;
+}
+
+a;
---
+++ test_data/output/ContinueStatement/tainted_4.js
@@ -0,0 +1,16 @@
+var a = 3;
+var b = document;
+var c = 5;
+
+while (b > 0) {
+    a;
+
+    if (a == 1) {
+        5;
+        continue;
+    }
+
+    a -= 1;
+}
+
+a;
