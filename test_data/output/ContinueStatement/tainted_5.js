b2471ada411503f815f64a5ee4a085bf1095c73d
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2026-01-01 17:44:31 -0500

    Added ContinueStatement.tainted_5.js Test

---
+++ test_data/output/ContinueStatement/tainted_5.js
@@ -0,0 +1,19 @@
+var a = 3;
+var b = document;
+var c = 5;
+
+while (b > 0) {
+    a;
+
+    while (c > 0) {
+        if (b == 2) {
+            continue;
+        }
+
+        c -= 1;
+    }
+
+    a -= 1;
+}
+
+a;
