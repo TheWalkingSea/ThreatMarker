39bc4078331bc0a21c2ee654b73d561b1cd36e5e
Austin McCallister <91487740+TheWalkingSea@users.noreply.github.com>
2026-01-01 14:28:37 -0500

    Added ReturnStatement.tainted_loop_return.js Test

---
+++ test_data/output/ReturnStatement/tainted_loop_return.js
@@ -0,0 +1,34 @@
+// Return in tainted environment with loops
+var global = 0;
+
+function taintedWhileReturn() {
+  var i = 0;
+  while (tainted) {
+    i++;
+    if (i > 2) {
+      return i;
+    }
+  }
+
+  // Not guaranteed to execute
+  global = 5;
+  return -1;
+}
+
+0;
+taintedWhileReturn();
+global;
+
+function taintedForReturn() {
+  var count = 10;
+  for (var j = 0; j < tainted; j++) {
+    if (j === 3) {
+      return j;
+    }
+    count++;
+  }
+  count;
+  return count;
+}
+
+taintedForReturn();
